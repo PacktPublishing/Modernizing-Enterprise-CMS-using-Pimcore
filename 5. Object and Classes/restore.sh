17f8c4228ab13b7b3f7fdaf821becc2e36269493
Marco Guiducci <m.guiducci@sintraconsulting.eu>
2021-01-11 22:01:05 +0100

    chapter scaffolding

---
+++ 5. Object and Classes/restore.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+composer install
+chown -R www-data .
+bin/console assets:install --symlink web
+bin/console pimcore:deployment:classes-rebuild -c
+bin/console cache:clear
+bin/console cache:warmup
+chown -R www-data .
