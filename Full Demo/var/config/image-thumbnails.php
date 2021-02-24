ceb52973e68967e6b40e1ba1267673f5e031a707
Daniele Fontani <>
2021-02-24 12:00:56 +0100

    slideshow implemented

---
+++ Full Demo/var/config/image-thumbnails.php
@@ -0,0 +1,56 @@
+<?php 
+
+return [
+    "MiniIcons" => [
+        "items" => [
+
+        ],
+        "medias" => [
+
+        ],
+        "name" => "MiniIcons",
+        "description" => "",
+        "group" => "",
+        "format" => "SOURCE",
+        "quality" => 85,
+        "highResolution" => NULL,
+        "preserveColor" => FALSE,
+        "preserveMetaData" => FALSE,
+        "rasterizeSVG" => FALSE,
+        "downloadable" => FALSE,
+        "modificationDate" => 1614163485,
+        "creationDate" => 1614163485,
+        "forcePictureTag" => FALSE,
+        "id" => "MiniIcons"
+    ],
+    "SlideShow" => [
+        "items" => [
+            [
+                "method" => "crop",
+                "arguments" => [
+                    "width" => 1920,
+                    "height" => 500,
+                    "x" => 0,
+                    "y" => 600
+                ]
+            ]
+        ],
+        "medias" => [
+
+        ],
+        "name" => "SlideShow",
+        "description" => "",
+        "group" => "",
+        "format" => "PNG",
+        "quality" => 85,
+        "highResolution" => 0.0,
+        "preserveColor" => FALSE,
+        "preserveMetaData" => FALSE,
+        "rasterizeSVG" => FALSE,
+        "downloadable" => FALSE,
+        "modificationDate" => 1614164332,
+        "creationDate" => 1614164220,
+        "forcePictureTag" => FALSE,
+        "id" => "SlideShow"
+    ]
+];
