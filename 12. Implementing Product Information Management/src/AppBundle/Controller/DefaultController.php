a9e50f4d7be53f685c85fd2789ba413063871dd0
Marco Guiducci <m.guiducci@sintraconsulting.eu>
2021-03-16 20:58:54 +0100

    chapter 12 scaffolding

---
+++ 12. Implementing Product Information Management/src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Pimcore\Controller\FrontendController;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DefaultController extends FrontendController
+{
+    /**
+     * The annotation will automatically resolve the view to MyController/myAnnotatedAction.html.twig
+     * 
+     * @Template() 
+     */    
+    public function defaultAction(Request $request)
+    {
+    }
+}
